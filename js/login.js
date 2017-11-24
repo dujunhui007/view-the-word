dfc885a6db60915e910cb2e44c3553de2b0dee7e
dujunhui007 <810743626@qq.com>
2017-11-24 14:30:19 +0800

    add myColumn html

---
+++ js/login.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+  // var password = $("#inputPassword").val();
+  $(".loginCenter .loginTo .loginToHome .loginBtn").click(function () {
+    // alert(password);
+    // window.location.href = "index.html?" + "password=" + password;
+    window.location.href = "myColumn.html";
+  })
+
+});
