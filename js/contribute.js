b6c6e6374f7b29766b9321c0e63a5c1dea4f8543
dujunhui007 <810743626@qq.com>
2017-12-05 15:37:44 +0800

    add ico content

--- js/contribute.js
+++ js/contribute.js
@@ -1,5 +1,49 @@
 $(document).ready(function () {
   $(".contributeContainer .contributeContainerCenter .contributeBtn button").click(function () {
-    window.location.href="publishArticle.html"
-  })
+    window.location.href = "publishArticle.html"
+  });
+
+  $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick1").click(function () {
+    $(".contributeContainer .contributeContainerCenter .columnBlockChain").css("display", "block");
+    $(".contributeContainer .contributeContainerCenter .columnBlockChain a").click(function () {
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick1").empty().append($(this).text());
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick2").empty().append("专栏");
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick3").empty().append("专栏");
+
+      $(".contributeContainer .contributeContainerCenter .columnBlockChain").css("display", "none");
+    });
+  });
+
+
+  $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick2").click(function () {
+    $(".contributeContainer .contributeContainerCenter .columnBlockChain").css("display", "block");
+    $(".contributeContainer .contributeContainerCenter .columnBlockChain a").click(function () {
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick2").empty().append($(this).text());
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick1").empty().append("专栏");
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick3").empty().append("专栏");
+
+      $(".contributeContainer .contributeContainerCenter .columnBlockChain").css("display", "none");
+    });
+  });
+
+  $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick3").click(function () {
+    $(".contributeContainer .contributeContainerCenter .columnBlockChain").css("display", "block");
+    $(".contributeContainer .contributeContainerCenter .columnBlockChain a").click(function () {
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick3").empty().append($(this).text());
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick1").empty().append("专栏");
+
+      $(".contributeContainer .contributeContainerCenter .contributeDetails ul li div span.columnClick2").empty().append("专栏");
+
+      $(".contributeContainer .contributeContainerCenter .columnBlockChain").css("display", "none");
+    });
+  });
+
 });
